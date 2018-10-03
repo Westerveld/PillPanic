d0f53ce5e002796a8ad553cc41c48ab6091babb7
Raheel Ahmad <4ahmar61@solent.ac.uk>
2018-10-03 15:33:47 +0100

    player script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public int playerSpeed = 2;
+
+    Rigidbody rigid;
+
+    Vector3 input;
+
+    public int playerSuffix;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal" + playerSuffix), 0, Input.GetAxis("Vertical" + playerSuffix));
+        rigid.velocity = new Vector3(input.x * playerSpeed, rigid.velocity.y, input.z * playerSpeed);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
