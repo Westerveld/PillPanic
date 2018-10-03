d40b7aeb841160207fdf79152db12d83be414d3a
Ethan Bruins <brudut93@gmail.com>
2018-10-03 15:42:05 +0100

    working on pickup

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory{
+
+    private int [] pillsInInventory;
+
+
+
+    public Inventory()
+    {
+
+        PillsInInventory = new int[5];
+
+    }
+
+    public void AddToInventory(PillType pill)
+    {
+        PillsInInventory[(int)pill]++;
+    }
+    
+
+
+    public int[] PillsInInventory
+    {
+        get
+        {
+            return pillsInInventory;
+        }
+
+        set
+        {
+            pillsInInventory = value;
+        }
+    }
+
+    public int CalculatePenaltyPoints(Inventory inv)
+    {
+        int error = 0;
+        for (int i = 0; i < PillsInInventory.Length; i++)
+            error += PillsInInventory[i] - Mathf.Abs(inv.PillsInInventory[i]);
+        return error * 10;
+    }
+
+
+
+
+}
+
+public enum PillType
+{
+
+    Goodnightol,
+    Xtacin,
+    Happillin,
+    Aliventillin,
+    Antipanicizole
+
+}
+
+
--- Assets/Scripts/PillTrigger.cs
+++ Assets/Scripts/PillTrigger.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PillTrigger : MonoBehaviour {
 
+public class PillTrigger : MonoBehaviour
+{
+
+	public PillType myPill;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
+
+	}
+
+	void OnCollisionStay(Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			PlayerController mController = col.gameObject.GetComponent<PlayerController>();
+			if (Input.GetButtonDown("Fire" + mController.playerSuffix))
+			{
+				
+			}
+		}
 	}
 }
